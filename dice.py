e7186012e55b1cd46772245c1133a17a9d395c0e
Johan Ramirez <johand200505@gmail.com>
2026-06-26 15:17:33 -0400

    Outputs random number when method roll_die is called

--- dice.py
+++ dice.py
@@ -1,9 +1,14 @@
 from random import randint
 
-class Die(sides=6):
-    def __init__(self, side):
+class Die():
+    def __init__(self, side=6):
         self.side = side
 
     def roll_die(self):
-        randint(1, self.side)
+        output = randint(1, self.side)
+        print(output)
+
+my_die = Die()
+my_die.roll_die()
+
 
